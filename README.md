3e1983f8283da9f047f85cc8667c2f6f0a949b17
catmmao <catmmao@gmail.com>
2019-02-08 15:20:25 +0800

    添加按钮组件，完成基本样式

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 用vue 写的ui组件，目的是为了熟悉vue
+
+使用本框架前请在CSS 中设置为border-box
+```$xslt
+box-sizing: border-box;
+```
---
+++ button.js
@@ -0,0 +1,6 @@
+//全局注册vue组件,叫g-button
+Vue.component('v-button', {
+    template: `
+        <button class="v-button">按钮</button>
+    `
+})
