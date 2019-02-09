fb6b8280dc101c0294f4fc9e5c8ec8240df2cf31
catmmao <catmmao@gmail.com>
2019-02-09 17:20:12 +0800

    添加 icon 和 iconPosition 属性

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,10 +1,20 @@
 <template>
-    <button class="v-button">按钮</button>
+    <button class="v-button" :class="{[`icon-${iconPosition}`]: true}">
+        <!-- 获取iconPosition的值，设置他的值为true，这样值就会被设置成class -->
+        <!--增加icon-->
+        <svg class="icon" v-if="icon"> <!-- 当icon 存在时才显示，避免使用者没有传入icon参数时还会出现这个标签 -->
+            <use :xlink:href=`#i-${icon}`></use> <!--xlink:href 前面一定要加冒号，代表是动态绑定-->
+        </svg>
+        <span class="text">
+            <slot></slot>
+        </span>
+    </button>
 </template>
 
 <script>
     export default {
-        name: "v-button"
+        name: "v-button",
+        props: ['icon', 'iconPosition'], //icon:图标类型； position:图标位置；
     }
 </script>
 
@@ -18,6 +28,10 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex;
+        vertical-align: middle;
+        align-items: center;
+        justify-content: center;
 
         &:hover {
             border-color: var(--border-color-hover);
@@ -30,6 +44,23 @@
         &:focus {
             outline: none;
         }
+
+        span.text {
+            margin: 0 2px;
+        }
+
+        &.icon-right {
+            > .icon {
+                order: 2;
+            }
+        }
+    }
+
+    /*设置icon样式*/
+    .icon {
+        /*宽度和高度设置为和字体一样高*/
+        width: 1em;
+        height: 1em;
     }
 
 </style>
