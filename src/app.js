3e90e215603faa7c706a88b6c69bab87dd852502
catmmao <catmmao@gmail.com>
2019-02-22 13:28:15 +0800

    添加input组件

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Button from './components/button' //引入button组件
 import Icon from './components/icon' //引入icon组件
 import ButtonGroup from './components/button-group'
+import Input from './components/input'
 
 //全局注册button, icon组件
 Vue.component('v-button', Button)
 Vue.component('v-icon', Icon)
 Vue.component('v-button-group', ButtonGroup)
+Vue.component('v-input', Input)
 new Vue({
     el: '#app',
     data: {
