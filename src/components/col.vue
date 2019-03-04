0e7c56d046b70a142a25af5cc32f3f3cebefe076
catmmao <catmmao@gmail.com>
2019-03-04 15:30:06 +0800

    基本实现col和row的功能

--- src/components/col.vue
+++ src/components/col.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <!--offset && offset-${offset} 里的 && 作用是：当 offset 的值在布尔值中为true时，才会增加这个 class，如果删掉 offset && 这部分，那么如果使用者没有传入
+    offset 参数时，会有 offset-undefined 这个 class -->
+    <div class="col" :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}" :class="[span && `col-${span}`, offset && `offset-${offset}`]">
+        <div style="border: 1px green solid;height: 100%;">
+            <slot></slot>
+        </div>
         <!--插槽：允许col组件内插入其他元素-->
     </div>
 </template>
@@ -12,6 +16,14 @@
             //占的比例
             span: {
                 type: [Number, String]
+            },
+            offset: {
+                type: [Number, String]
+            }
+        },
+        data() {
+            return {
+                gutter: 0
             }
         }
     }
@@ -21,8 +33,8 @@
     .col {
         height: 100px;
         width: 50%;
-        border: 1px red solid;
 
+        /*计算每列宽度*/
         /*前缀是 col- */
         $class-prefix: col-;
         /*-- scss 的插值语法 # {}  类似js插值语法 ${} */
@@ -30,6 +42,15 @@
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
                 width: ($n / 24) * 100%;
+            }
+        }
+
+        /*计算间隔*/
+        /*前缀是 offset 变量可以重复使用*/
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24) * 100%;
             }
         }
     }
--- src/components/row.vue
+++ src/components/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft: -gutter/2 + 'px',marginRight: -gutter/2 + 'px'}">
         <slot></slot>
         <!--插槽：允许div里面插入其他元素 如 col组件 -->
     </div>
@@ -7,7 +7,19 @@
 
 <script>
     export default {
-        name: "row"
+        name: "row",
+        props: {
+            gutter: {
+                type: [Number, String]
+            }
+        },
+        mounted() {
+            //mounted后，该组件的所有子元素也挂载完毕，可以通过this.$children 获取他们
+            this.$children.forEach((vm) => {
+                //vm代表每一个子元素本身 把row的gutter传递给他
+                vm.gutter = this.gutter
+            })
+        }
     }
 </script>
 
