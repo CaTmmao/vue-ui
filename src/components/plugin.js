9a04d36fa698c11a7a3364228940287c0035f635
catmmao <catmmao@gmail.com>
2019-04-04 12:58:23 +0800

    实现手动关闭、以HTML渲染内容的功能

--- src/components/plugin.js
+++ src/components/plugin.js
@@ -6,12 +6,16 @@ import Toast from './toast/toast'
 export default {
     //里面有个install方法，告诉vue如何安装这个插件 里面有两个参数Vue & options
     install(Vue, options) {
-        //在vue的原型链上加上一个toast方法
-        Vue.prototype.$toast = function(message) {
+/*        在vue的原型链上加上一个toast方法 该方法是一个函数，接收两个参数
+        第一个参数是需要弹出的内容，第二个参数是一个对象，对象的属性是toast组件中props对象中的属性*/
+        Vue.prototype.$toast = function(message, toastOptions) {
             //创建一个toast的vue构造器
             let Constructor = Vue.extend(Toast)
             //new一个toast实例
-            let toast = new Constructor()
+            let toast = new Constructor({
+                //将用户传入的对象传到toast实例中去
+                propsData: toastOptions
+            })
             //toast的slot标签中的默认内容是传入的message参数
             toast.$slots.default = message
             //把toast实例mount到内存中 这句是必须的，否则toast所有生命周期的钩子都不会执行
--- src/components/toast/toast.vue
+++ src/components/toast/toast.vue
@@ -1,19 +1,122 @@
 <template>
-    <div class="toast">
-        <!--slot标签里传入想要弹出的内容-->
-        <slot></slot>
+    <div class="toast" ref="toast">
+        <div class="message">
+            <!--slot标签里传入想要弹出的内容 slot标签不能用v-html 如果用户想让传入的内容以HTML的方式渲染，那么就传入
+            enableHTML参数为true-->
+            <slot v-if="!enableHTML"></slot>
+            <!-- 如果用户想把弹出的内容以HTML方式显示就传入enableHTML:true
+            在plugin.js中，用户传入的message数组被传入了toast组件的$slots.default中，所以这里当作
+                 HTML渲染时，值取的是this(toast组件实例)中$slots.default的内容-->
+            <div v-else v-html="this.$slots.default"></div>
+        </div>
+
+        <!--竖线-->
+        <div class="line" ref="line"></div>
+
+        <!--关闭按钮-->
+        <span v-if="closeButton" class="close" @click="clickCloseButton">
+            <!--关闭按钮的内容，默认是 [关闭]-->
+            {{closeButton.text}}
+        </span>
     </div>
 </template>
 
 <script>
+    /* export的对象是构造组件的选项
+    相当于 Vue.component('xxx', {
+        name: 'xx'
+    }) 中的第二个参数这个对象一样
+     */
     export default {
-        name: 'v-toast'
+        name: 'v-toast',
+        props: {
+            //自动关闭
+            autoClose: {
+                type: Boolean,
+                default: true
+            },
+            //自动关闭延迟时间
+            autoCloseDelay: {
+                type: Number,
+                default: 3
+            },
+            //关闭按钮
+            closeButton: {
+                type: Object,
+                /* 虽然这个属性是对象类型，但是default不能写成对象类型，要用一个函数返回一个对象，是因为如果页面中
+                多次使用该对象，那么都会变成都引用同一对象，当其中一个发生变化时，其他的也会发生变化，因此这里需要用一个函数返回对象*/
+                default() {
+                    return {
+                        //关闭按钮默认的文字为[关闭]
+                        text: '关闭',
+                        //回调函数默认没有，如果用户需要在按钮关闭完成后执行回调，就传入一个callback参数
+                        callback: undefined
+                    }
+                }
+            },
+            //关闭按钮的内容是否以HTML方式渲染 由于用HTML渲染会有风险，所以默认是false
+            enableHTML: {
+                type: Boolean,
+                default: false
+            }
+        },
+        mounted() {
+            /* 将toast的高度赋值给分隔线 注意：下面的style只能获取到内联样式不能获取到css样式
+             因此不能用 this.$refs.toast.style.height直接赋值
+             如 this.$refs.line.style.height = this.$refs.toast.style.height
+             需要用异步接收css，因为在plugin.js中，是先mount，再把元素加到body中
+             */
+            this.lineHeight()
+
+            //调用自动关闭函数
+            this.autoCloseFn()
+        },
+        methods: {
+            //自动关闭函数
+            autoCloseFn() {
+                //当 autoClose 为 true，同时不存在关闭按钮才执行
+                if (this.autoClose && !this.closeButton) {
+                    setTimeout(() => {
+                        this.close()
+                    }, this.autoCloseDelay * 1000)
+                }
+            },
+            //关闭
+            close() {
+                //把元素从页面中移除 这句一定要写 destroy并不会把元素从页面中删掉
+                this.$el.remove()
+                //销毁组件
+                this.$destroy()
+            },
+            //点击关闭按钮
+            clickCloseButton() {
+                //关闭toast
+                this.close()
+                //如果用户传了按钮关闭后的回调，触发关闭按钮的回调
+                if (this.closeButton.callback) {
+                    this.closeButton.callback()
+                }
+            },
+            //给分隔线设置高度
+            lineHeight() {
+                /* 由于在plugin.js中，我们的toast实例是先mount，再被append到body中，因此在mounted钩子函数中还无法
+                获取到页面中的toast高度，这里需要异步 */
+                this.$nextTick(() => {
+                    /*
+                    由于this.$refs.toast.style只能获取到HTML中的内联样式，无法获取到toast在网页中
+                    实际的height，因此我们不能通过下面的方式进行赋值
+                    this.$refs.line.style.height = this.$refs.toast.style.height
+                     */
+                    this.$refs.line.style.height = `${this.$refs.toast.getBoundingClientRect().height}px`
+                })
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
     $font-size: 14px;
-    $toast-height: 40px;
+    $toast-min-height: 40px;
     $toast-bg: rgba(0, 0, 0, .75);
     .toast {
         background: $toast-bg;
@@ -23,7 +126,7 @@
         padding: 0 16px;
         box-shadow: 0 0 3px 0 rgba(0, 0, 0, .5);
         font-size: $font-size;
-        height: $toast-height;
+        min-height: $toast-min-height;
         display: flex;
         align-items: center;
         line-height: 1.8;
@@ -31,5 +134,20 @@
         top: 10px;
         left: 50%;
         transform: translateX(-50%);
+
+        .message {
+            padding: 7px 0;
+        }
+
+        .line {
+            border-left: 1px solid #666;
+            margin: 0 16px;
+            height: 100%;
+        }
+
+        .close {
+            //禁止文字收缩
+            flex-shrink: 0;
+        }
     }
 </style>
