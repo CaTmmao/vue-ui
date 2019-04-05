6f6e682ae99d061e9d8fbadb9a181c2a9bc0ba83
catmmao <catmmao@gmail.com>
2019-04-05 18:52:27 +0800

    tabs组件初始化

---
+++ src/components/tabs/tabs-body.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tabs-body">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-tabs-body'
+    }
+</script>
+
+<style scoped lang="scss">
+    .tabs-body {
+
+    }
+</style>
---
+++ src/components/tabs/tabs-head.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tabs-head">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-tabs-head'
+    }
+</script>
+
+<style scoped lang="scss">
+    .tabs-head {
+
+    }
+</style>
