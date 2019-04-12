7f0b0975e5b95f8c02cc6f132a00a18d16cd552e
catmmao <catmmao@gmail.com>
2019-04-12 18:26:52 +0800

    collapse基本样式和功能初始化

---
+++ src/components/collapse/collapse-item.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="collapse-item">
+        <div class="title" @click="open=!open">
+            {{title}}
+        </div>
+        <div class="content" v-if="open">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-collapse-item',
+        props: {
+            title: {
+                type: String,
+                required: true
+            }
+        },
+        data() {
+            return {
+                open: false
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .collapse-item {
+
+        .title {
+            margin: -1px;
+            border: 1px solid #ddd;
+            padding: 0 8px;
+            min-height: 32px;
+            display: flex;
+            align-items: center;
+        }
+
+        &:first-child {
+            .title {
+                border-top-left-radius: 4px;
+                border-top-right-radius: 4px;
+            }
+        }
+
+        &:last-child {
+            //当最后一个元素中的title同时也是最后一个内容，也就是title后面没有其他内容时就应用该样式
+            .title:last-child {
+                border-bottom-left-radius: 4px;
+                border-bottom-right-radius: 4px;
+            }
+        }
+
+        .content {
+            padding: 8px;
+        }
+    }
+
+</style>
---
+++ src/components/collapse/collapse.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="collapse">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-collapse'
+    }
+</script>
+
+<style scoped lang="scss">
+    .collapse {
+        border: 1px #ddd solid;
+        border-radius: 4px;
+        margin: 100px;
+    }
+</style>
